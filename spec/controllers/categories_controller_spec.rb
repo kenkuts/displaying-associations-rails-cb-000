4a64d1c26cb1fa9e089e014d26a5ffbf8f5d3818
Kenneth Kutschera <kennethchoi19@gmail.com>
2019-07-15 20:17:59 +0000

    Automatically backed up by Learn

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,5 +1,5 @@
 # require 'rails_helper'
-# 
+#
 # RSpec.describe CategoriesController, type: :controller do
 #   describe "GET #show" do
 #     it "returns HTTP success" do
@@ -7,7 +7,7 @@
 #       expect(response).to have_http_status(:success)
 #     end
 #   end
-# 
+#
 #   describe "GET #edit" do
 #     it "returns HTTP success" do
 #       get :edit, id: @category
